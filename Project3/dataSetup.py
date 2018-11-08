69f3c503fdecb7a4e9459dfd564763c0a3aa8e5f
AlexanderMeulemans <alexander.meulemans@gmail.com>
2018-11-08 21:18:05 +0100

    Feature extraction pipeline in order

--- Project3/dataSetup.py
+++ Project3/dataSetup.py
@@ -1,9 +1,15 @@
 import numpy as np
 
-y = np.genfromtxt("y_train.csv", delimiter=",", skip_header=1)
-X = np.genfromtxt("X_train.csv", delimiter=",", skip_header=1)
-X_test = np.genfromtxt("X_test.csv", delimiter=",", skip_header=1)
+X_len = 5118
+X_test_len = 3412
 
+X = []
+X_test = []
+for i in range(X_len-1):
+    sample = np.genfromtxt("X_train.csv", delimiter=",", skip_header=1+i,skip_footer=X_len-i-2)
+    X.append(sample[1:])
+for i in range(X_test_len):
+    sample = np.genfromtxt("X_test.csv", delimiter=",", skip_header=1 + i, skip_footer=X_test_len - i - 2)
+    X_test.append(sample[1:])
+y = np.genfromtxt("y_train.csv", delimiter=",", skip_header=1)
 y = np.ravel(y[:,1:])
-X = X[:,1:]
-X_test = X_test[:,1:]
--- Project3/featureSelection.py
+++ Project3/featureSelection.py
@@ -1,1 +1,8 @@
-i=5
+import sys
+sys.path.append(r"c:\users\alexander\appdata\local\programs\python\python36\lib\site-packages")
+import numpy as np
+from biosppy.signals import ecg
+from dataSetup import X,y
+
+
+i = 5
