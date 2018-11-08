16676d8b4e4b9e674a1e50e8ba8b05261f7c2a8f
AlexanderMeulemans <alexander.meulemans@gmail.com>
2018-11-08 18:13:08 +0100

    from now on we ignore .csv files!

---
+++ Project3/dataSetup.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+y = np.genfromtxt("y_train.csv", delimiter=",", skip_header=1)
+X = np.genfromtxt("X_train.csv", delimiter=",", skip_header=1)
+X_test = np.genfromtxt("X_test.csv", delimiter=",", skip_header=1)
+
+y = np.ravel(y[:,1:])
+X = X[:,1:]
+X_test = X_test[:,1:]
