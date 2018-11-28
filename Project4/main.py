6947c919883a4c88cff989a06012d3cb1b865591
Alexander Meulemans <alexander.meulemans@gmail.com>
2018-11-28 13:14:51 +0100

    tensorflow video classification code

--- Project4/main.py
+++ Project4/main.py
@@ -2,13 +2,16 @@ from get_data import get_videos_from_folder,get_target_from_csv
 import os
 import numpy as np
 from utils import save_solution
+import sys
+sys.path.append(r"C:\Users\Alexander\Anaconda3\envs\complete_environment\Lib\site-packages\ffprobe")
+sys.path.append(r"C:\Users\Alexander\Anaconda3\envs\complete_environment\Lib\site-packages\ffmpeg")
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-train_folder = os.path.join(dir_path,"../train/")
-test_folder = os.path.join(dir_path,"../test/")
+train_folder = os.path.join(dir_path,r"train\\")
+test_folder = os.path.join(dir_path,r"test\\")
 
-train_target = os.path.join(dir_path,'../train_target.csv')
-my_solution_file = os.path.join(dir_path,'../solution.csv')
+train_target = os.path.join(dir_path,r'train_target.csv')
+my_solution_file = os.path.join(dir_path,r'solution.csv')
 
 x_train = get_videos_from_folder(train_folder)
 y_train = get_target_from_csv(train_target)
