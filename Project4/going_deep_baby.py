e6c8ad6081b4429927ef69477243c52a3a4fabcb
AllanSCosta <allanc@mit.edu>
2018-11-29 18:04:54 +0100

    deep learning is hard

--- Project4/going_deep_baby.py
+++ Project4/going_deep_baby.py
@@ -1,3 +1,4 @@
+
 import os
 import tensorflow as tf
 import numpy as np
@@ -5,6 +6,7 @@ from tf_utils import input_fn_from_dataset,input_fn_frame_from_dataset,save_tf_r
 from get_data import get_videos_from_folder,get_target_from_csv
 from utils import save_solution
 
+from sklearn import preprocessing
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.callbacks import EarlyStopping
 from tensorflow.python.keras.layers import Dropout, Dense, Conv3D, Conv2D, MaxPooling2D, TimeDistributed, LSTM, Flatten
@@ -23,22 +25,7 @@ my_solution_file = os.path.join(dir_path,'./solution.csv')
 
 y = get_target_from_csv(train_target)
 X_train = get_videos_from_folder(train_folder)
-x_test = get_videos_from_folder(test_folder)
-
-def build_cnn():
-    model = Sequential()
-    model.add(Conv2D(128, 3, input_shape=(100, 100, 1), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=2))
-    model.add(Flatten())
-    return model
-
-def build_lstm(cnn):
-    model = Sequential()
-    model.add(TimeDistributed(cnn))
-    model.add(LSTM(5, input_shape=(50, 50)))
-    model.add(Dense(1, activation="sigmoid"))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+X_test = get_videos_from_folder(test_folder)
 
 
 time_size = 22
@@ -53,11 +40,11 @@ def model():
                               input_shape=(time_size, img_width, img_height, img_channels)))
     model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(1, 1))))
 
-    model.add(TimeDistributed(Conv2D(128, (4, 4), activation='relu', padding='same')))
-    model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
-
-    model.add(TimeDistributed(Conv2D(256, (4, 4), activation='relu')))
-    model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
+    # model.add(TimeDistributed(Conv2D(128, (4, 4), activation='relu', padding='same')))
+    # model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
+    #
+    # model.add(TimeDistributed(Conv2D(256, (4, 4), activation='relu')))
+    # model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
 
     model.add(TimeDistributed(Flatten()))
 
@@ -87,7 +74,7 @@ for i in range(10):
     print(y_pred[i], y[i])
 print("\n\n loss: " + str(roc_auc))
 
-print(net.evaluate(X_train, y))
+# print(net.evaluate(X_train, y))
 
 
 
