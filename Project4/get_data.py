c05d37d73fb7694153413736382a30ad6a520598
AllanSCosta <allanc@mit.edu>
2018-12-03 15:34:06 +0100

    fixed model

--- Project4/get_data.py
+++ Project4/get_data.py
@@ -4,15 +4,14 @@ from sklearn import preprocessing
 
 skvideo.setFFmpegPath("/usr/local/bin")
 import skvideo.io
-import os 
-import sys 
+import os
+import sys
 import pandas as pd
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 def get_videos_from_folder(data_folder):
-	data_folder = os.path.join(dir_path,data_folder)
-	file_names, x = [], []
-	scaler = preprocessing.StandardScaler()
+	data_folder = os.path.join(dir_path, data_folder)
+	file_names, x, y_copies = [], [], []
 
 	if os.path.isdir(data_folder):
 		for dirpath, dirnames, filenames in os.walk(data_folder):
@@ -21,10 +20,23 @@ def get_videos_from_folder(data_folder):
 				statinfo = os.stat(file_path)
 				if statinfo.st_size != 0:
 					video = skvideo.io.vread(file_path, outputdict={"-pix_fmt": "gray"})[:, :, :, 0]
-					video = np.expand_dims(video[:22,:,:], axis=3)
-					x.append(video)
+					length = video.shape[0]
+					number_of_splits = length // 20
+					y_copies += [number_of_splits]
+					for i in range(number_of_splits):
+						start = i * 20
+						x.append(np.expand_dims(video[start: start + 20,:,:], axis=3))
 					file_names.append(int(filename.split(".")[0]))
-	return np.asarray(x)
+
+	return np.asarray(x), y_copies
+
+def get_full_data(X_folder, y_file):
+	x, y_copies = get_videos_from_folder(X_folder)
+	basis_y, processed_y = get_target_from_csv(y_file), []
+	for i in range(len(basis_y)):
+		processed_y += [basis_y[i]] * y_copies[i]
+
+	return x, np.asarray(processed_y)
 
 def get_target_from_csv(csv_file):
 	csv_file = os.path.join(dir_path, csv_file)
